8c62651fcc68c873d26ac12bfd2c210a8dcd93fc
jngbn <[user email]>
2025-09-11 14:33:37 +0900

    Initial commit from IntelliJ

---
+++ src/test/java/jngbnss/board/BoardApplicationTests.java
@@ -0,0 +1,13 @@
+package jngbnss.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
